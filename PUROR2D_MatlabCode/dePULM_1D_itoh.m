2fe75c62464dad79216c9ed5531f21df1e03ae9a
Tim Whelan <twhelan5@uwo.ca>
2017-10-19 12:27:24 -0400

    Fixes xr vs yr mixup and a rounding error

--- PUROR2D_MatlabCode/dePULM_1D_itoh.m
+++ PUROR2D_MatlabCode/dePULM_1D_itoh.m
@@ -13,17 +13,17 @@ for index_y = 1:yr_tmp
     phase_itoh_x(index_y,:) = unwrap(phase_1D_tmp);
     else
     phase_1D_tmp = phase_original(index_y,xr_tmp:-1:1);
-    phase_itoh_x(index_y,xr_tmp:-1:1) = unwrap(phase_1D_tmp);        
+    phase_itoh_x(index_y,xr_tmp:-1:1) = unwrap(phase_1D_tmp);
     end
 end
 %
 phase_itoh_y = zeros(yr_tmp,xr_tmp);
 for index_x = 1:xr_tmp
-        if (index_x/2 - round(index_x)/2)== 0
+        if (index_x/2 - round(index_x/2))== 0
         phase_1D_tmp = phase_original(:,index_x);
         phase_itoh_y(:,index_x) = unwrap(phase_1D_tmp);
         else
-        phase_1D_tmp = phase_original(xr_tmp:-1:1,index_x);
-        phase_itoh_y(xr_tmp:-1:1,index_x) = unwrap(phase_1D_tmp);            
+        phase_1D_tmp = phase_original(yr_tmp:-1:1,index_x);
+        phase_itoh_y(yr_tmp:-1:1,index_x) = unwrap(phase_1D_tmp);
         end
 end
