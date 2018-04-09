3b44fa1c28f9c8ef63e41430f3ec6873c169a075
Tim Whelan <twhelan5@uwo.ca>
2018-04-09 14:03:52 -0400

    Reorganize and rename mask_generation for clarity

--- Mask4PUROR/mask_generation.m
+++ Mask4PUROR/mask_generation.m
@@ -1,34 +1,35 @@
-%
-function [mask4unwrap, mask4supp, mask4stack] = mask_generation(complex_image,th4noise,th4unwrap,th4supp,th4stack)
-
+function [mask4unwrap, mask4supp, mask4stack] = mask_generation(complex_image,noise_threshold,unwrap_threshold,support_threshold,stack_threshold)
+    
     matrix_size = size(complex_image);
+    
     if length(matrix_size) == 2
         matrix_size(3) = 1;
     end
-
+    
     H = fspecial('gaussian',3);
-
+    
     mask4unwrap = zeros(matrix_size(1),matrix_size(2),matrix_size(3));
     mask4supp = zeros(matrix_size(1),matrix_size(2),matrix_size(3));
     mask4stack = zeros(matrix_size(1),matrix_size(2),matrix_size(3));
     
     for index_slice = 1:matrix_size(3)
-        complex_tmp(:,:) = complex_image(:,:,index_slice);
+        background_noise = noise_threshold(index_slice);
+        
+        complex_tmp = complex_image(:,:,index_slice);
         mag_original = abs(complex_tmp);
-        background_noise = th4noise(index_slice);
         mask_tmp = ones(matrix_size(1),matrix_size(2));
-        mask_tmp(mag_original < background_noise*th4stack) = 0;
+        mask_tmp(mag_original < background_noise*stack_threshold) = 0;
         mask4stack(:,:,index_slice) = mask_tmp;
         
         complex_tmp = imfilter(complex_tmp,H);
         complex_tmp(isnan(complex_tmp)) = 0;
         mag_original = abs(complex_tmp);
         mask_tmp_0 = ones(matrix_size(1),matrix_size(2));
-        mask_tmp_0(mag_original < background_noise*th4unwrap) = 0;
+        mask_tmp_0(mag_original < background_noise*unwrap_threshold) = 0;
         mask4unwrap(:,:,index_slice) = mask_tmp_0;
         
         mask_tmp = ones(matrix_size(1),matrix_size(2));
-        mask_tmp(mag_original < background_noise*th4supp) = 0;
+        mask_tmp(mag_original < background_noise*support_threshold) = 0;
         mask_tmp(mask_tmp_0 == 0) = 0;
         mask4supp(:,:,index_slice) = mask_tmp;
     end
